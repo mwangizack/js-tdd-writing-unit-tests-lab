d61630f351740ada986d95fc920bf935761013f5
Zacchaeus Ndirangu Mwangi <ndirangu.zmwangi@gmail.com>
2024-06-24 18:29:43 +0300

    feat: Define function isPalindrome

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,16 @@
 // Your tests here
+import { isPalindrome } from "../utils";
+
+describe("isPalindrome", () => {
+  it("Returns true for a word that is the same in reverse, and false for a word that is not", () => {
+    const word = "racecar";
+    const result = isPalindrome(word);
+    expect(result).toBe(true);
+  });
+
+  it('Returns false for car', () => {
+    const word = "car";
+    const result = isPalindrome(word);
+    expect(result).toBe(false);
+  })
+});
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,5 @@
 // Your code here
+export function isPalindrome(word){
+    const reversedWord = [...word].reverse().join('');
+    return word === reversedWord;
+}
